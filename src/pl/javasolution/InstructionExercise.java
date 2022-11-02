1368d09de8174275be2b3d9c5d4e6c1f5549f5c4
Awantura <karolkolano96@gmail.com>
2022-11-02 08:59:04 +0100

    sprawdzenie warunku if, else if, else

---
+++ src/pl/javasolution/InstructionExercise.java
@@ -0,0 +1,13 @@
+package pl.javasolution;
+
+public class InstructionExercise {
+    public static void main(String[] args) {
+        int value = 13;
+        if (value % 2 == 0) {
+            System.out.println("Parzysta");
+        }
+        else{
+            System.out.println("Nieparzysta");
+        }
+    }
+}

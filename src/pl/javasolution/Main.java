a10bcab7417f20cd5119b0b3e18e60c13750c102
Awantura <karolkolano96@gmail.com>
2022-11-02 08:26:14 +0100

    skanowanie tekstu uzytkownika

--- src/pl/javasolution/Main.java
+++ src/pl/javasolution/Main.java
@@ -1,8 +1,13 @@
 package pl.javasolution;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoj wiek: ");
+        int value = scanner.nextInt();
+        System.out.println("Masz " + value + " lat.");
     }
 }

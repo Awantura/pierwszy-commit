06dc97e86738c6e7d68e7c27e5e95accae39266c
Awantura <karolkolano96@gmail.com>
2022-11-02 09:52:33 +0100

    wartosci procentowe

--- src/pl/javasolution/InstructionExercise.java
+++ src/pl/javasolution/InstructionExercise.java
@@ -9,5 +9,29 @@ public class InstructionExercise {
         else{
             System.out.println("Nieparzysta");
         }
+
+
+        double percentage = 20.0;
+         if (percentage >= 0 && percentage < 20){
+             System.out.println("Ocena niedostateczna.");
+         }
+         else if (percentage >= 20 && percentage < 40){
+             System.out.println("Ocena dopuszczajaca.");
+         }
+         else if (percentage >= 40 && percentage < 60){
+             System.out.println("Ocena dostateczna.");
+         }
+         else if (percentage >= 60 && percentage < 80){
+             System.out.println("Ocena dobra.");
+         }
+         else if (percentage >= 80 && percentage < 90){
+             System.out.println("Ocena bardzo dobra.");
+         }
+         else if (percentage >= 90 && percentage < 100){
+             System.out.println("Ocena celujaca.");
+         }
+         else {
+             System.out.println("Bledne dane.");
+         }
     }
 }

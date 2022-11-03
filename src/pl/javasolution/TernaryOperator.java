8dd2593eba89d1b943bce2e1de12ef6cd1613c5c
Awantura <karolkolano96@gmail.com>
2022-11-03 14:58:27 +0100

    Operator warunkowy

--- src/pl/javasolution/TernaryOperator.java
+++ src/pl/javasolution/TernaryOperator.java
@@ -1,2 +1,12 @@
-package pl.javasolution;public class TernaryOperator {
+package pl.javasolution;
+
+import java.util.Scanner;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoj wiek: ");
+        int age = scanner.nextInt();
+        System.out.println(age >= 18 ? "Jestes pelnoletni!" : "Do pelnoletnosci brakuje Ci " + (18 - age) + " lat!");
+    }
 }

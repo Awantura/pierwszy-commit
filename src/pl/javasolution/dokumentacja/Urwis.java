a39a2da051a3a2c36120e26475cb6aa287aee933
Awantura <karolkolano96@gmail.com>
2023-02-08 20:47:36 +0100

    kolejne petle

---
+++ src/pl/javasolution/dokumentacja/Urwis.java
@@ -0,0 +1,11 @@
+package pl.javasolution.dokumentacja;
+
+public class Urwis {
+    public static void main(String[] args) {
+        final String firma = "PPHU Kids Time Robert Falek";
+        final int rokZalozenia = 2011;
+        final int liczbaPracownikow = 13;
+
+
+    }
+}
--- src/pl/javasolution/szkolenie4/zadania/zad14.java
+++ src/pl/javasolution/szkolenie4/zadania/zad14.java
@@ -1,7 +1,41 @@
 package pl.javasolution.szkolenie4.zadania;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class zad14 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
 
+        int positive = 0;
+        int negative = 0;
+        int biggestNumber = Integer.MIN_VALUE;
+        int smallestNumber = Integer.MAX_VALUE;
+        int number = 0;
+        do {
+            System.out.println("Wpisz ile liczb wygenerowac :");
+            number = scanner.nextInt();
+        } while (number < 2);
+        for (int i = 0; i < number; i++) {
+            int randomNumber = rand.nextInt(200) - 100;
+            if (randomNumber > 0) {
+                positive++;
+                if (randomNumber > biggestNumber) {
+                    biggestNumber = randomNumber;
+                }
+            }
+            if (randomNumber < 0) {
+                negative++;
+                if (randomNumber < smallestNumber) {
+                    smallestNumber = randomNumber;
+                }
+            }
+        }
+        System.out.println("Najwieksza liczba to " + biggestNumber);
+        System.out.println("Najmniejsza liczba to " + smallestNumber);
+        System.out.println("Ilosc dodatnich liczb to " + positive);
+        System.out.println("Ilosc ujemnych liczb to " + negative);
     }
 }
+
